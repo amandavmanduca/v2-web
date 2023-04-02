9cdbe9f5fe1fccb32297b460ecb84af5efb1c239
Amanda Manduca <amandavmanduca@hotmail.com>
2023-04-02 13:44:21 -0300

    :tada: Initial commit

--- next.config.js
+++ next.config.js
@@ -4,3 +4,16 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+
+module.exports = withPWA({
+	pwa: {
+    register: true,
+    disable: process.env.NODE_ENV === 'development',
+    skipWaiting: true,
+	},
+})
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/appstyles/globals.css'
+import { ApolloProvider } from '@apollo/client'
+import { client } from '@app/server/apollo/client'
+import '@app/styles/Home.module.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
